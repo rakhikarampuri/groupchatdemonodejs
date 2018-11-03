abb35834605c831c9f882ae03f36a1fc980d77ab
Karampuri Rakesh <rakhikarampuri@gmail.com>
2018-11-03 12:48:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var socket = require('socket.io');
+var app = express();
+var server = app.listen(2000, function () {
+    console.log("start");
+});
+app.use(express.static('public'));
+
+var io = socket(server);
+
+io.on('connection', function (socket) {
+    console.log("socket", socket.id);
+
+    socket.on('chat', function (data) {
+        io.sockets.emit('chat', data);
+    });
+    socket.on('typing', function (data) {
+        io.sockets.emit('typing', data);
+    });
+
+})
